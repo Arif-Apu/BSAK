97fa8421e62d7f8875d2fd7d9f2972098f688f8a
mamunur121 <mamun.ruet.ete@gmail.com>
2018-01-08 16:59:29 +0100

    mistakenly added other's part
    back to original

--- employeeAction.js
+++ employeeAction.js
@@ -135,30 +135,6 @@ exports.findByUsername = function(username, cb) {
   });
     };
 
-// Submitting contactUs/contactform form details
-
-exports.addContactUsForm = function(req, res){
-
-    usr = req.body;
-    console.log(usr);
-    var details = {
-        firstname: usr.firstname,
-        lastname: usr.lastname,
-        email: usr.email,
-        subject: usr.subject,
-        message: usr.message
-    };
-    db.query('INSERT into  `contactus` SET ?', details, function (err, result) {
-        if (err)
-            throw err;
-        else{
-            console.log(' The value inserted in database. ');
-            req.flash('success', 'We received your feedback, we will reply as soon as possible');
-            res.redirect('/contactform');
-        }
-    });
-
-};
 
 
 //Finding by the userId
